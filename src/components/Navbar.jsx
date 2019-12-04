d6c7a636cad944e4ac46ebd340aef37fa8e59d8e
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2019-12-04 12:19:48 +0000

    Add basic useEffect toggle class assignment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Navbar = () => {
   const [darkMode, setDarkMode] = useState(false);
+
   const toggleMode = e => {
     e.preventDefault();
     setDarkMode(!darkMode);
   };
+
+  useEffect(() => {
+    // if darkMode = true, then assign class name dark-mode
+    if (darkMode === true) {
+      document.querySelector(`body`).classList.add("dark-mode");
+    } else {
+      document.querySelector(`body`).classList.remove("dark-mode");
+    }
+
+    // darkMode = value to be set to the local storage
+  });
 
   return (
     <nav className="navbar">
